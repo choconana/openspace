38df3aa5b16c0a6d15e1b5ee02c5234eaf07fa19
coco <1285015889@qq.com>
2024-07-18 23:27:55 +0800

    modify

--- section10/web3-wallet/src/store/EtherClient/index.tsx
+++ section10/web3-wallet/src/store/EtherClient/index.tsx
@@ -1,7 +1,7 @@
 import { createPublicClient, http } from 'viem';
 import { mainnet } from 'viem/chains';
 
-export const publicClient = createPublicClient({
+export const client = createPublicClient({
   chain: mainnet,
-  transport: http(),
-});
+  transport: http("https://eth-mainnet.g.alchemy.com/v2/ToDOPYbbyBKiCdbWdhYBf6FrS3M23oAk"),
+})
