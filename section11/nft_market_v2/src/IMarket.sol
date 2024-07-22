b4e9cb4433a3ac4d6949d5ab282741a49b9de5a8
coco <1285015889@qq.com>
2024-07-22 23:01:54 +0800

    add nft trade offline

--- section11/nft_market_v2/src/IMarket.sol
+++ section11/nft_market_v2/src/IMarket.sol
@@ -6,9 +6,17 @@ interface IMarket {
 
     event Buy(address indexed buyer, uint256 indexed tokenId, uint256 price);
 
+    event Cancel(bytes32 orderHash);
+
     error AmountIncorrect(address account, uint256 tokenId, uint256 price);
 
     error NFTOwnerIncorrect(address sender, uint256 tokenId, address owner);
 
     error InvalidSigner(address sender);
+
+    error TimeExpired(address sender, uint256 currentTime, uint256 deadline);
+
+    error InvalidAddress();
+
+    error OrderCanceled(bytes32 orderHash);
 }
